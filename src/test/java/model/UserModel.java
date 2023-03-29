d1be7b55c21129d55956835856bf9a40eec79158
Abdullah Al Noman <a_a_noman93@yahoo.com>
2023-03-30 00:51:06 +0600

    Update UserModel.java
    'update transaction model'

--- src/test/java/model/UserModel.java
+++ src/test/java/model/UserModel.java
@@ -27,11 +27,7 @@ public class UserModel {
         this.role = role;
     }
 
-    public UserModel(String from_account,String to_account,int amount){
-        this.from_account = from_account;
-        this.to_account = to_account;
-        this.amount = amount;
-    }
+    
 
 
 }
