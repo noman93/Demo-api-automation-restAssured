794913868345dbaa39cc1e7c0bc6dba683737ab8
Abdullah Al Noman <a_a_noman93@yahoo.com>
2023-03-30 00:48:31 +0600

    Update TransactionModel.java
    'upload'

--- src/test/java/model/TransactionModel.java
+++ src/test/java/model/TransactionModel.java
@@ -3,7 +3,6 @@ package model;
 public class TransactionModel {
     public String from_account;
     public String to_account;
-
     public int amount;
     public TransactionModel(String from_account,String to_account,int amount){
         this.from_account = from_account;
